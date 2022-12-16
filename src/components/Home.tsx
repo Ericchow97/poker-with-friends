3aba1507a04051154768fef8015ae194300db94b
Ericchow97 <36716562+Ericchow97@users.noreply.github.com>
2022-12-15 20:25:28 -0500

    Initial Commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
